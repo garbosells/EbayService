c3ce41417d60bdd1b309b35dcd935821aac3d069
Erica Anderson <Erica.Anderson@drivetime.com>
2019-03-18 16:12:36 -0700

    Cleaned up some stuff + added some notes

--- EbayService/Managers/AuthorizationManager.cs
+++ EbayService/Managers/AuthorizationManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eBay.ApiClient.Auth.OAuth2.Model;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Options;
 
 namespace EbayService.Managers
 {
     public class AuthorizationManager
     {
+        TelemetryClient telemetryClient = new TelemetryClient();
+        private readonly IOptions<AppSettings> settings;
 
     }
 }
