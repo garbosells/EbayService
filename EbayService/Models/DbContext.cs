43832efbe027f7494945f44e4d144352b1122ce8
Erica Anderson <anderson.erica.lee@gmail.com>
2019-03-26 14:26:27 -0700

    added db context

---
+++ EbayService/Models/DbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayService.Models
+{
+    public class DbModelContext : DbContext
+    {
+        public DbModelContext(DbContextOptions<DbModelContext> options)
+    : base(options)
+        { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CompanyEbayAuth> CompanyEbayAuths { get; set; }
+    }
+
+    public class Company
+    {
+        public long companyId { get; set; }
+        public long companyEbayAuthId { get; set; }
+    }
+
+    public class CompanyEbayAuth
+    {
+        public long companyEbayAuthId { get; set; }
+        public string userToken { get; set; }
+        public DateTime userTokenExpiration { get; set; }
+        public string refreshToken { get; set; }
+        public DateTime refreshTokenExpiration { get; set; }
+    }
+}
+
