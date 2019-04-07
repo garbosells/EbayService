e8ea32699aa573c55dd20c9e1794311396b1feb5
Erica Anderson <anderson.erica.lee@gmail.com>
2019-04-06 23:18:40 -0700

    even more models for days

--- EbayService/Models/ItemModels/ClothingItem.cs
+++ EbayService/Models/ItemModels/ClothingItem.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace EbayService.Models.ItemModels
 {
+  /// <summary>
+  /// Represents a Clothing Item created in the app
+  /// </summary>
   public class ClothingItem : AppItem
   {
-
+    public ClothingSize Size { get; set; }
+    public ClothingMaterial Material { get; set; }
+    public Gender ClothingItemGender { get; set; }
+    public Color PrimaryColor { get; set; }
+    public Color SecondaryColor { get; set; }
+    public Era Era { get; set; }
   }
 }
