447a69373985525e13423924bcec1456ed4c8d9a
Erica Anderson <anderson.erica.lee@gmail.com>
2019-04-17 07:57:28 -0700

    put items back as they were before

--- EbayService/Models/ItemModels/ItemAttribute.cs
+++ EbayService/Models/ItemModels/ItemAttribute.cs
@@ -8,13 +8,14 @@ namespace EbayService.Models.ItemModels
   /// <summary>
   /// Represents an attribute of a given item. An item's attributes are based on its category.
   /// </summary>
-  public class ItemAttribute<T>
+  public class ItemAttribute
   {
     public long AttributeId { get; set; } //uniquely identifies this attribute for this item
     public long CategoryAttributeId { get; set; } //identifies the category attribute that goes with this -> need to use this to map to Ebay/Etsy item attributes
     public bool Required { get; set; }
     public int ListOrder { get; set; } //determines the order by which the user is prompted for this value in the app
     public string AttributeDescription { get; set; } //used to prompt the user, ie Brand, Style, etc
-    public T AttributeValue { get; set; }
+    public Type AttributeValueType { get; set; }
+    public dynamic AttributeValue { get; set; }
   }
 }
