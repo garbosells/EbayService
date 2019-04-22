3043a6ad86afb7359fb216cde06a292a475f17c9
Erica Anderson <Erica.Anderson@drivetime.com>
2019-04-22 08:53:52 -0700

    committing what works before i change everything

---
+++ EbayService/Util/KeyStore.cs
@@ -0,0 +1,24 @@
+﻿using EbayService.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayService.Util
+{
+    public class KeyStore
+    {
+        private readonly IConfiguration configuration;
+
+        public KeyStore(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public EbayOAuthToken UserToken(string companyId)
+        {
+            
+        }
+    }
+}
