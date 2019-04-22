4c9293f0f58413cfc2b1e49f1122cfe29228d647
Erica Anderson <Erica.Anderson@drivetime.com>
2019-04-22 08:08:53 -0700

    key vault setup, cleaned up config

--- EbayService/Program.cs
+++ EbayService/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace EbayService
 {
@@ -20,6 +23,18 @@ namespace EbayService
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseApplicationInsights()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                        azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                config.AddAzureKeyVault(
+                    $"https://ebay-key-vault.vault.azure.net/",
+                    keyVaultClient,
+                    new DefaultKeyVaultSecretManager());
+            })
                 .UseStartup<Startup>();
     }
 }
